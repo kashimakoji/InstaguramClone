4977312130fc88e6633c7c48c27c4fc4a2d4fc22
atsushi oshima <aboutagial@yahoo.co.jp>
2021-09-02 21:42:26 +0900

    ログイン機能作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(mail: params[:session][:mail].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
+    else
+      flash.now[:danger] = 'passwordまたはemailが違います'
+      render :new
+    end
+  end
+
 end
